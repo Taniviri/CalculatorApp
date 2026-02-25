1d8e8c90d1d56c19237417d1bd36301d33917728
Пупкин Василий Иванович <example@example.com>
2026-02-25 18:22:24 +0500

    Добавлено взаимодействие с пользователем

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+
 namespace ConsoleApp
 {
     internal class Program
@@ -17,24 +19,61 @@ namespace ConsoleApp
                 string op = Console.ReadLine();
 
                 Console.Write("\nВведите первое число: ");
-                double a = double.Parse(Console.ReadLine());
+                if (!double.TryParse(Console.ReadLine(), out double a))
+                {
+                    Console.WriteLine("Ошибка: некорректный ввод первого числа!");
+                    continue;
+                }
+
                 Console.Write("Введите второе число: ");
-                double b = double.Parse(Console.ReadLine());
+                if (!double.TryParse(Console.ReadLine(), out double b))
+                {
+                    Console.WriteLine("Ошибка: некорректный ввод второго числа!");
+                    continue;
+                }
+
                 double result = 0;
+                bool operationValid = true;
+
                 switch (op)
                 {
-                    case "+": result = Calculator.Sum(a, b); break;
-                    case "-": result = Calculator.Substract(a, b); break;
-                    case "*": result = Calculator.Multiply(a, b); break;
+                    case "+":
+                        result = Calculator.Sum(a, b);
+                        break;
+                    case "-":
+                        result = Calculator.Subtract(a, b); // Исправлено имя метода
+                        break;
+                    case "*":
+                        result = Calculator.Multiply(a, b);
+                        break;
                     case "/":
-                        if (b != 0) result = Calculator.Divide(a, b);
-                        else Console.WriteLine("Ошибка: деление на ноль!");
+                        if (b != 0)
+                            result = Calculator.Divide(a, b);
+                        else
+                        {
+                            Console.WriteLine("Ошибка: деление на ноль!");
+                            operationValid = false;
+                        }
                         break;
                     default:
                         Console.WriteLine("Неизвестная операция!");
+                        operationValid = false;
                         continue;
                 }
-                Console.WriteLine($"Результат: {result}");
+
+                if (operationValid)
+                    Console.WriteLine($"Результат: {result}");
             }
         }
     }
+
+    // Класс Calculator с необходимыми методами
+    internal static class Calculator
+    {
+        public static double Sum(double a, double b) => a + b;
+        public static double Subtract(double a, double b) => a - b;
+        public static double Multiply(double a, double b) => a * b;
+        public static double Divide(double a, double b) => a / b;
+    }
+}
+
